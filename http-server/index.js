924c4a9ac8ed380911b5b97849bb6ae1bc29ba6e
root <root@DESKTOP-6LGIQQI>
2023-03-23 13:21:31 +0530

    Level 2

---
+++ http-server/index.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = require("minimist")(process.argv.slice(2), {
+  alias: {
+    port: "name",
+  },
+ });
+
+let regContent = "";
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+fs.readFile("registration.html", (err, reg) => {
+  if (err) {
+    throw err;
+  }
+  regContent = reg;
+});
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/reg":
+        response.write(regContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(PORT);
