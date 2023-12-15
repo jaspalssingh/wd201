8a68e8d8d8bbf69a20cc63f4f3eda6597f7f5993
jaspal <hp@DESKTOP-RDJ3R68>
2023-12-15 23:09:41 +0530

    level 4 milestone

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,13 +1,14 @@
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
-
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+const today = new Date().toISOString().split("T")[0];
+const time = new Date();
 describe("ToDo Test Suite", () => {
   beforeAll(() => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().split("T")[0],
+      dueDate: today,
     });
   });
   test("Should add new todo", () => {
@@ -16,7 +17,7 @@ describe("ToDo Test Suite", () => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().split("T")[0],
+      dueDate: today,
     });
     expect(all.length).toBe(length + 1);
   });
@@ -24,5 +25,31 @@ describe("ToDo Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Should Display overdue items", () => {
+    add({
+      title: "Test1",
+      completed: false,
+      dueDate: new Date(time.getTime() - 1 * 60 * 60 * 24 * 1000)
+        .toISOString()
+        .split("T")[0],
+    });
+    const items = overdue();
+    expect(items.length).toBe(1);
+  });
+  test("Should Display due today items", () => {
+    const items = dueToday();
+    expect(items.length).toBe(2);
+  });
+  test("Should Display due later items", () => {
+    add({
+      title: "Test1",
+      completed: false,
+      dueDate: new Date(time.getTime() + 1 * 60 * 60 * 24 * 1000)
+        .toISOString()
+        .split("T")[0],
+    });
+    const items = dueLater();
+    expect(items.length).toBe(1);
   });
 });
